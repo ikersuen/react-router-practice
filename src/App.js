f28a41963162c3c46de44a1b5cb630fafb1d14f0
Iker Suen <suenik@oocl.com>
2018-12-06 14:04:43 +0800

    [iker] feat: add about and home page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import AboutPage from './components/AboutPage'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router>
           <div>
+            <nav>
             <ul>
               <li>
                 <Link to="/about">about</Link>
               </li>
+              <li>
+                <Link to="/">home</Link>
+              </li>
             </ul>
-            <Route path="/about" component={() => <h2>about</h2>}></Route>
+            </nav>
+            <hr/>
+            <Route path="/" exact component={() => <h1>home page</h1>} />
+            <Route path="/about" component={AboutPage}/>
           </div>
-        </Router>
-      </div>
     );
   }
 }
